a4434b01fb1fa7a818a106b5c1ad3df0c7ca7ef5
agam <duahimanshu100@gmail.com>
2016-12-14 23:34:32 +0530

    PostDensityApi

--- analyticsApi/urls.py
+++ analyticsApi/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from analyticsApi.api.post import PostListApi,PostHistoryListApi,PostFilterUsageApi,PostTagUsageApi,PostGeolocationApi
+from analyticsApi.api.post import PostListApi,PostHistoryListApi,PostFilterUsageApi,PostTagUsageApi,PostGeolocationApi,PostDensityApi
 urlpatterns = [
     url(r'^(?i)api/(?P<profile_id>.+)/Posts$', PostListApi.as_view()),
 
     url(r'^(?i)api/(?P<profile_id>.+)/Posts/History$', PostHistoryListApi.as_view()),
     url(r'^(?i)api/(?P<profile_id>.+)/Posts/PostFilterUsage$', PostFilterUsageApi.as_view()),
-    url(r'^(?i)api/(?P<profile_id>.+)/Posts/PostTagUsageApi', PostTagUsageApi.as_view()),
-url(r'^(?i)api/(?P<profile_id>.+)/Posts/PostGeolocationApi', PostGeolocationApi.as_view()),
+    url(r'^(?i)api/(?P<profile_id>.+)/Posts/PostTagUsage$', PostTagUsageApi.as_view()),
+    url(r'^(?i)api/(?P<profile_id>.+)/Posts/PostGeolocation$', PostGeolocationApi.as_view()),
+    url(r'^(?i)api/(?P<profile_id>.+)/Posts/PostDensity$', PostDensityApi.as_view()),
+
 
     ]
