fa3210255401ed045991fb3313681d3a3d70012d
agam <duahimanshu100@gmail.com>
2016-12-25 13:59:21 +0530

    celery discover tasks

--- analytics/settings.py
+++ analytics/settings.py
@@ -129,3 +129,6 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
+
+
+CELERY_IMPORTS = ('analyticsApi.tasks.analyticsTask')
