85baec4510b1b85fcfc1bff13340a45e56c3fa33
agam <duahimanshu100@gmail.com>
2017-01-16 09:47:23 +0530

    MostRecentPostApi

--- analyticsApi/api/engagement.py
+++ analyticsApi/api/engagement.py
@@ -1,9 +1,12 @@
-from analyticsApi.serializers import PostMetricSerializer
+import dateutil.parser
+from analyticsApi.serializers import PostMetricSerializer, PostsListSerializer
 from django.db.models import Sum, Avg
 from rest_framework import generics
 from rest_framework.response import Response
 from analyticsApi.utility import Utility
+from analyticsApi.models import PostMetric
 from django.db import connection
+from django.db.models import Max
 
 
 class PostMetricListApi(generics.ListAPIView):
@@ -70,3 +73,42 @@ class ProfileCommentHistoryApi(generics.ListAPIView):
             return Response(result)
         finally:
             cursor.close()
+
+
+class MostRecentPostApi(generics.ListAPIView):
+    '''
+    List post and post count by profile
+    '''
+    serializer_class = PostsListSerializer
+    model = serializer_class.Meta.model
+    paginate_by = 100
+
+    def get_queryset(self):
+        profile_id = self.kwargs['profile_id']
+        queryset = self.model.objects.filter(
+            profile_id=profile_id)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        limit_by = int(self.request.query_params.get('limit_by', 5))
+        queryset = self.get_queryset()
+        # 2016-12-02T17:00:25.910711
+        from_date = self.request.query_params.get('from_date', None)
+        to_date = self.request.query_params.get('to_date', None)
+        filter = self.request.query_params.get('filter', None)
+
+        if from_date:
+            from_date = dateutil.parser.parse(from_date)
+            queryset = queryset.filter(created_at__gte=from_date)
+        if to_date:
+            to_date = dateutil.parser.parse(to_date)
+            queryset = queryset.filter(created_at__lte=to_date)
+
+        if filter:
+            queryset = queryset.filter(primary_content_type=filter)
+
+        queryset = queryset.order_by('-created_at')[:limit_by]
+        # post_metrics = PostMetric.objects(post_id__in=queryset, is_latest=True)
+        # serializer = PostMetricSerializer(post_metrics, many=True)
+        # return Response(serializer.data
+        return Response(queryset)
--- analyticsApi/urls.py
+++ analyticsApi/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from analyticsApi.api.post import PostListApi, PostHistoryListApi
 from analyticsApi.api.post import PostFilterUsageApi, PostDistributionApi, PostTagRepartitionApi
 from analyticsApi.api.post import PostTagUsageApi, PostGeolocationApi, PostDensityApi
-from analyticsApi.api.engagement import PostMetricListApi,ProfileLikeHistoryApi,ProfileCommentHistoryApi
+from analyticsApi.api.engagement import PostMetricListApi, ProfileLikeHistoryApi, ProfileCommentHistoryApi, MostRecentPostApi
 
 urlpatterns = [
     url(r'^(?i)api/(?P<profile_id>.+)/Posts$', PostListApi.as_view()),
@@ -27,7 +27,7 @@ urlpatterns = [
         ProfileLikeHistoryApi.as_view()),
     url(r'^(?i)api/(?P<profile_id>.+)/Posts/engagement/ProfileCommentHistory$',
         ProfileCommentHistoryApi.as_view()),
-
-
+    url(r'^(?i)api/(?P<profile_id>.+)/Posts/engagement/MostRecentPost$',
+        MostRecentPostApi.as_view()),
 
 ]
