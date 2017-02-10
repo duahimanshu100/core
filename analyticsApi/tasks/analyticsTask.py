d60868558f4b278dd73da7a02b6befdf9b5cfbc5
agam <duahimanshu100@gmail.com>
2017-02-10 22:42:46 +0530

    created every min task for follower

--- analyticsApi/tasks/analyticsTask.py
+++ analyticsApi/tasks/analyticsTask.py
@@ -1,8 +1,8 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from analyticsApi.models import Profile, SmAccount, ProfileLike
-from analyticsApi.serializers import ProfileSerializer, PostSerializer, ProfileLikeSerializer
+from analyticsApi.models import Profile, SmAccount, ProfileLike, ProfileMetric
+from analyticsApi.serializers import ProfileSerializer, PostSerializer, ProfileLikeSerializer,ProfileMetricSerializer
 from analyticsApi.simplyMeasured.api.analytics.analytics import ApiAnalytics
 from analyticsApi.utility import Utility
 from analyticsApi.simplyMeasured.api.token.token import ApiToken
@@ -10,10 +10,8 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from datetime import datetime
 
-# TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0ZW5hbnQiOiJzaW1wbHltZWFzdXJlZC1wcm9kIiwiZGV2aWNlIjoiQnJvd3NlciIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgdXNlcl9pZCB1c2VyX21ldGFkYXRhIiwiZW1haWwiOiJkdWFoaW1hbnNodTEwMEBnbWFpbC5jb20iLCJhcHBfbWV0YWRhdGEiOnsiaXNfc21fYWRtaW4iOmZhbHNlLCJhcGlfYWNjZXNzIjp0cnVlfSwidXNlcl9tZXRhZGF0YSI6eyJlbWFpbCI6ImR1YWhpbWFuc2h1MTAwQGdtYWlsLmNvbSIsImZpcnN0X25hbWUiOiJIaW1hbnNodSIsImxhc3RfbmFtZSI6IkR1YSJ9LCJhY2NvdW50X2lkIjoiZmQ3YTg0OGEtYmZhOS00YzI0LWI5ZTctYjEwNDllMmQxMzZlIiwicmF0ZWxpbWl0Ijp7Im1vbnRoIjoxMDAwMDAsIm1pbnV0ZSI6NTAwfSwidXNlcl9pZCI6ImF1dGgwfDgyMjNhOTkyLTNhY2MtNDUxNS04OGJlLTYxNmQ3M2M2ODE3YyIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJ1cGRhdGVkX2F0IjoiMjAxNi0xMi0xNVQwNjoyNDoxMS42OTBaIiwiaWRlbnRpdGllcyI6W3sidXNlcl9pZCI6IjgyMjNhOTkyLTNhY2MtNDUxNS04OGJlLTYxNmQ3M2M2ODE3YyIsInByb3ZpZGVyIjoiYXV0aDAiLCJjb25uZWN0aW9uIjoiVUFNREItUHJvZHVjdGlvbiIsImlzU29jaWFsIjpmYWxzZX1dLCJjcmVhdGVkX2F0IjoiMjAxNi0xMS0yNlQxOTowMDo1Ni42NzhaIiwiaXNzIjoiaHR0cHM6Ly9zaW1wbHltZWFzdXJlZC1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw4MjIzYTk5Mi0zYWNjLTQ1MTUtODhiZS02MTZkNzNjNjgxN2MiLCJhdWQiOiJQd0RzTDRyUlY3enZHczNoYXdRQXI3NUhabElTaWZLTiIsImV4cCI6MTQ4MjQ1NTg2OSwiaWF0IjoxNDgyNDE5ODY5LCJhenAiOiJtU2QzSUIzbnBnelcyNW5HbjBJeHkxU2VMN1YyRUtLRSJ9.awZ3ijvb1r7fKRJZAQPh1H6L7OiUdKRnZ32CHveKocU'
 
-# @shared_task
-def syncProfiles():
+def syncProfiles(is_hourly=False):
     '''
     SyncProfiless will create or update the profiles
     of simply measured  associated with the token and account id
@@ -29,6 +27,9 @@ def syncProfiles():
         r = Utility.save_and_update_data(
             ProfileSerializer, result, Profile, 'profile_id', 'profile_id')
         print(r)
+        if is_hourly:
+            ProfileMetric.objects.filter(is_latest=True).update(is_latest=False)
+            Utility.save_and_update_data(ProfileMetricSerializer, result, ProfileMetric)
     else:
         print('Token Not Found')
 
@@ -43,9 +44,11 @@ def syncAllProfilesPost():
     count = 0
     for profile in Profile.objects.filter(is_active=True):
         count = count + 1
-        print('Profile Id Sync Starts for ' + str(profile.id) + ' at ' + str(datetime.now()))
+        print('Profile Id Sync Starts for ' +
+              str(profile.id) + ' at ' + str(datetime.now()))
         syncProfilePosts(profile, TOKEN)
-        print('Profile Id Sync Completed for ' + str(profile.id) + ' at ' + str(datetime.now()))
+        print('Profile Id Sync Completed for ' +
+              str(profile.id) + ' at ' + str(datetime.now()))
         print('Count is ' + str(count))
 
 
@@ -113,7 +116,13 @@ def syncSinglePost(post):
     obj = ApiAnalytics(TOKEN)
     # obj.get_posts_by_profile(profile, None, params)
 
+
 @periodic_task(run_every=(crontab(minute=0, hour='*/1')), name="syncAllProfileAndPost", ignore_result=True)
 def syncAllProfileAndPost():
-    syncProfiles()
+    syncProfiles(is_hourly=True)
     syncAllProfilesPost()
+
+
+@periodic_task(run_every=(crontab()), name="syncAudienceCount", ignore_result=True)
+def syncAudienceCount():
+    syncProfiles()
