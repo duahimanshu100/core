30b6360a943f61e94b57b7e0f9e96f961153fef8
Ubuntu <ubuntu@ip-172-31-31-30.ap-south-1.compute.internal>
2017-02-20 19:34:53 +0000

    All Apis

--- analytics/settings.py
+++ analytics/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # CELERY SETTINGS
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
